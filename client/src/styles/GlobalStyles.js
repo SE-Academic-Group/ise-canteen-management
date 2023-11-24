0305a31def1b5b13926badd0c5f7f24262753444
Trương Minh Phát <tmphat1312@gmail.com>
2023-11-24 12:35:51 +0700

    style: Add visually hidden class and reset image by default

--- client/src/styles/GlobalStyles.js
+++ client/src/styles/GlobalStyles.js
@@ -168,7 +168,6 @@ h5,
 h6 {
   overflow-wrap: break-word;
   hyphens: auto;
-  /* text-wrap: balance; */
 }
 
 img {
@@ -181,6 +180,18 @@ img {
   shape-margin: 0.75rem;
 
   filter: grayscale(var(--image-grayscale)) opacity(var(--image-opacity));
+}
+
+.sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border-width: 0;
 }
 `;
 
