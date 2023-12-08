e6279ebbfa9572f9ff547b00991fc40fa712faeb
21120502 <37113714+Flashback054@users.noreply.github.com>
2023-12-08 21:00:24 +0700

    Fix userId not found on querying reviews. Populate reviews.

--- backend/src/controllers/review.controller.js
+++ backend/src/controllers/review.controller.js
@@ -2,16 +2,26 @@ const Review = require("../models/review.model");
 const ControllerFactory = require("./controller.factory");
 const ApiFeatures = require("../utils/apiFeatures");
 
-exports.getAllReviews = ControllerFactory.getAll(Review);
+exports.getAllReviews = ControllerFactory.getAll(Review, {
+	populate: [
+		{ path: "productId", select: "name" },
+		{ path: "userId", select: "name email" },
+	],
+});
 exports.getReview = ControllerFactory.getOne(Review);
-exports.createReview = async (req, res, next) => create(Review);
+exports.createReview = ControllerFactory.createOne(Review);
 exports.updateReview = ControllerFactory.updateOne(Review);
 exports.deleteReview = ControllerFactory.deleteOne(Review);
 
-exports.setProductAndUserIds = (req, res, next) => {
-	// Allow nested routes
-	if (!req.body.product) req.body.product = req.params.productId;
-	if (!req.body.user) req.body.user = req.user.id;
+exports.setProductAndUserOnBody = (req, res, next) => {
+	if (!req.body.productId) req.body.productId = req.params.productId;
+	if (!req.body.userId) req.body.userId = req.user?.id;
 
-	next();
+	return next();
+};
+
+exports.setProductOnQuery = (req, res, next) => {
+	if (!req.query.productId) req.query.productId = req.params.productId;
+
+	return next();
 };
