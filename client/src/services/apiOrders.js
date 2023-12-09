86233bcbdf634c513418581cc62e2108e02a151c
Trương Minh Phát <tmphat1312@gmail.com>
2023-12-09 13:58:52 +0700

    refactor: Adapt order service to new factory

--- client/src/services/apiOrders.js
+++ client/src/services/apiOrders.js
@@ -1,35 +1,10 @@
-import axios from "axios";
+import {
+  getItemFactory,
+  getItemsFactory,
+  createEditItemFactory,
+} from "../utils/apiItemsFactory";
 
-import { PAGE_SIZE, BACKEND_URL } from "../utils/constants";
-
-const RESOURCE_URL = BACKEND_URL + "/orders";
-
-export async function getOrders({ page = 1, filters = [], sortBy }) {
-  sortBy = sortBy || { sort: "orderDate", order: "desc" };
-
-  // TODO: refactor this code (extract to a helper function)
-  const url = new URL(RESOURCE_URL);
-  url.searchParams.append("_page", page);
-  url.searchParams.append("_limit", PAGE_SIZE);
-  url.searchParams.append("_sort", sortBy.sort);
-  url.searchParams.append("_order", sortBy.order);
-
-  filters?.forEach((filter) => {
-    url.searchParams.append(filter.field, filter.value);
-  });
-
-  const res = await axios(url.toString());
-  const count = res.headers["x-total-count"];
-
-  return { data: res.data, count };
-}
-
-export async function getOrder(orderId) {
-  const res = await axios(`${RESOURCE_URL}/${orderId}`);
-  return res.data;
-}
-
-export async function updateOrder(orderId, order) {
-  const res = await axios.patch(`${RESOURCE_URL}/${orderId}`, order);
-  return res.data;
-}
+export const getOrders = getItemsFactory("orders");
+export const getOrder = getItemFactory("orders");
+export const createOrder = createEditItemFactory("orders");
+export const updateOrder = createEditItemFactory("orders");
