a8844aa3da8a0f71bcbc796abd1859349f2d573b
Trương Minh Phát <tmphat1312@gmail.com>
2023-12-10 09:44:11 +0700

    app: Setup deposit page

---
+++ client/src/pages/Deposit.jsx
@@ -0,0 +1,11 @@
+import PageHeading from "../ui/PageHeading";
+
+function Products() {
+  return (
+    <>
+      <PageHeading>Nạp tiền vào tài khoản</PageHeading>
+    </>
+  );
+}
+
+export default Products;
