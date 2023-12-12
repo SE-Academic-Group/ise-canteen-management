66fb6ad81c5f315c9cafe8c0f383e82df3e532b7
Trương Minh Phát <tmphat1312@gmail.com>
2023-12-12 15:47:57 +0700

    docs: Add doc to the function

--- client/src/utils/apiFeatures.js
+++ client/src/utils/apiFeatures.js
@@ -1,3 +1,17 @@
+/**
+ * Builds URL parameters based on the provided resource name and options.
+ * @param {string} resourceName - The name of the resource.
+ * @param {object} options - The options for building the URL parameters.
+ * @param {number} options.page - The page number.
+ * @param {number} options.limit - The limit of items per page.
+ * @param {Array} options.filters - The array of filters.
+ * @param {string} options.filters.field - The field to filter by.
+ * @param {string} options.filters.value - The value to filter by.
+ * @param {string} options.sortBy.sort - The field to sort by.
+ * @param {string} options.sortBy.order - The sort order ("asc" or "desc").
+ * @param {string} options.q - The search query.
+ * @returns {string} The built URL with parameters.
+ */
 export function buildUrlParams(resourceName, options) {
   const { page, limit, filters = [], sortBy, q } = options;
 
