1b104896ccedae85646ea55aa49c60d2cb18c579
Trương Minh Phát <tmphat1312@gmail.com>
2023-12-12 15:41:20 +0700

    refactor: Add explicit statements and remove dependency on PAGE_SIZE

--- client/src/utils/apiFeatures.js
+++ client/src/utils/apiFeatures.js
@@ -1,22 +1,23 @@
-import { PAGE_SIZE } from "../constants/api";
-export function buildUrlParams(resource, options) {
-  const { page, limit = PAGE_SIZE, filters, sortBy, q } = options;
+export function buildUrlParams(resourceName, options) {
+  const { page, limit, filters = [], sortBy, q } = options;
 
   const searchParams = new URLSearchParams();
+  const connector = resourceName.includes("?") ? "&" : "?";
 
   page && searchParams.append("page", page);
   limit && searchParams.append("limit", limit);
-  filters &&
-    filters.forEach((filter) => {
-      searchParams.append(filter.field, filter.value);
-    });
+  q && searchParams.append("q", q);
+
+  filters.forEach((filter) => {
+    searchParams.append(filter.field, filter.value);
+  });
 
   if (sortBy && sortBy.sort && sortBy.order) {
     const prefix = sortBy.order === "asc" ? "" : "-";
     searchParams.append("sort", `${prefix}${sortBy.sort}`);
   }
 
-  q && searchParams.append("q", q);
+  const url = resourceName + connector + searchParams.toString();
 
-  return resource + "?" + searchParams.toString();
+  return url;
 }
