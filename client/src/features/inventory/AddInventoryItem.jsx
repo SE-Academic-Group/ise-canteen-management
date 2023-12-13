30d9719e4dbf5027fd93d6169e1b695b0691f27d
Trương Minh Phát <tmphat1312@gmail.com>
2023-12-13 13:13:49 +0700

    app: No need for batch import and export

--- client/src/features/inventory/AddInventoryItem.jsx
+++ client/src/features/inventory/AddInventoryItem.jsx
@@ -1,34 +1,17 @@
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
-import FlexContainer from "../../ui/FlexContainer";
 import CreateInventoryItemForm from "./CreateInventoryItemForm";
 
 export default function AddInventoryItem() {
   return (
     <div>
       <Modal>
-        <FlexContainer>
-          <Modal.Open opens="create-item">
-            <Button>Thêm hàng</Button>
-          </Modal.Open>
-          <Modal.Open opens="import-items">
-            <Button>Nhập lô hàng</Button>
-          </Modal.Open>
-          <Modal.Open opens="export-items">
-            <Button>Xuất lô hàng</Button>
-          </Modal.Open>
-        </FlexContainer>
+        <Modal.Open opens="create-item">
+          <Button>Thêm hàng</Button>
+        </Modal.Open>
 
         <Modal.Window name="create-item">
           <CreateInventoryItemForm />
-        </Modal.Window>
-
-        <Modal.Window name="import-items">
-          <div>Import</div>
-        </Modal.Window>
-
-        <Modal.Window name="export-items">
-          <div>Import</div>
         </Modal.Window>
       </Modal>
     </div>
