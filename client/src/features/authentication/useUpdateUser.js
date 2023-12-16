3bfe44c8d08e1edd4db9fdc531719c80ae97c34e
Trương Minh Phát <tmphat1312@gmail.com>
2023-12-16 19:10:09 +0700

    app: Add user query key

--- client/src/features/authentication/useUpdateUser.js
+++ client/src/features/authentication/useUpdateUser.js
@@ -1,17 +1,12 @@
-import { useQueryClient } from "@tanstack/react-query";
+import { QUERY_KEYS } from "../../constants/keys";
 import { useApiMutation } from "../../hooks/useApiMutation";
 import { updateCurrentUser } from "../../services/apiAuth";
 
 export function useUpdateUser() {
-  const queryClient = useQueryClient();
-
   const { isLoading: isUpdating, mutate: updateUser } = useApiMutation({
     fn: updateCurrentUser,
-    key: ["user"],
+    key: QUERY_KEYS.USER,
     successMsg: "Cập nhật thông tin thành công!",
-    onSuccess: ({ user }) => {
-      queryClient.setQueryData(["user"], user);
-    },
   });
 
   return { updateUser, isUpdating };
