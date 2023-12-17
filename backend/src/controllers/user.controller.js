023f18c19737ea4d46898439b4b06f12fdf5c22a
21120502 <37113714+Flashback054@users.noreply.github.com>
2023-12-17 16:44:17 +0700

    Allow querying inactive users for admin

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,6 +4,7 @@ const ControllerFactory = require("./controller.factory");
 const multerUpload = require("../utils/multerUpload");
 const sharp = require("sharp");
 const AppError = require("../utils/appError");
+const APIFeatures = require("../utils/apiFeatures");
 
 const User = require("../models/user.model");
 
@@ -31,8 +32,69 @@ exports.createUser = async (req, res, next) => {
 	});
 };
 
-exports.getAllUsers = ControllerFactory.getAll(User);
-exports.getUser = ControllerFactory.getOne(User);
+exports.getAllUsers = async (req, res, next) => {
+	// Allow filtering by today's date
+	// Loops through query string, find any key that has a value of "today"
+	// and replace it with an object that specifies the range of today's date
+	Object.keys(req.query).forEach((key) => {
+		if (req.query[key] === "today") {
+			// Filter orders by today's date
+			const today = new Date();
+			today.setHours(0, 0, 0, 0);
+			const tomorrow = new Date(today);
+			tomorrow.setDate(tomorrow.getDate() + 1);
+			req.query[key] = {
+				$gte: today,
+				$lt: tomorrow,
+			};
+		}
+	});
+
+	// EXECUTE QUERY
+	const query = User.find().select("+active");
+	query.includeInactive = true;
+	const features = new APIFeatures(query, req.query)
+		.filter()
+		.limitFields()
+		.paginate()
+		.sort();
+
+	// Added .lean() to improve performance
+	const docs = await features.query.lean();
+	const count = await User.countDocuments(features.filterObj);
+
+	// SEND RESPONSE
+	// Set X-Total-Count header
+	res.set("Access-Control-Expose-Headers", "X-Total-Count");
+	res.set("X-Total-Count", count);
+	res.status(200).json({
+		status: "success",
+		data: docs,
+	});
+};
+exports.getUser = async (req, res, next) => {
+	const query = User.findById(req.params.id).select("+active").lean();
+	query.includeInactive = true;
+
+	const doc = await query;
+
+	if (!doc) {
+		throw new AppError(
+			404,
+			"NOT_FOUND",
+			`Không tìm thấy user với ID ${req.params.id}`,
+			{
+				id: req.params.id,
+				modelName: "user",
+			}
+		);
+	}
+
+	res.status(200).json({
+		status: "success",
+		data: doc,
+	});
+};
 exports.updateUser = async (req, res, next) => {
 	const user = await User.findByIdAndUpdate(req.params.id, req.body, {
 		new: true,
